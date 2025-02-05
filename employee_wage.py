b247f24eb15f258c4f88e6a178e629e8c28022cf
Dharshan <dharshan1436@gmail.com>
2025-02-05 10:01:56 +0530

    UC2 Calculate daily Employee wage

--- employee_wage.py
+++ employee_wage.py
@@ -7,4 +7,14 @@ def check_attendance():
     else:
         print("Employee is Absent")
 
+def calculate_daily_wage(wage_per_hour, full_day_hours):
+    attendance = random.randint(0, 1)
+    if attendance == 1:
+        daily_wage = wage_per_hour * full_day_hours
+        print(f"Employee is Present. Daily Wage: {daily_wage}")
+    else:
+        print("Employee is Absent. Daily Wage: 0")
+
+
 check_attendance()
+calculate_daily_wage(20, 8)
